a0fa53eea32d9a1ad28647077b0a8c756e9901ba
ColemanDDimmer <cdimmer@sundownbg.com>
2024-03-09 21:15:43 -0600

    Changes to some pages.

---
+++ Midterm/get-go.js
@@ -0,0 +1,30 @@
+fetch("./data.json")
+  .then((response) => response.json())
+  .then((myData) => loadLocations(myData));
+
+function loadLocations(myData) {
+  var mainContainer = document.getElementById("GetGoLocs");
+  for (var i = 0; i < myData.GetGo.length; i++) {
+    let name = myData.GetGo[i].name;
+    let url = myData.GetGo[i].url;
+    let desc = myData.GetGo[i].desc;
+    // let url = myMovies.movies[i].url;
+    let div = document.createElement("div");
+    div.innerHTML = `
+                                <div class="row featurette">
+                                    <div class="col-md-7 order-md-2">
+                                        <h2 class="featurette-heading fw-normal lh-1"
+                                            style="text-align: center; margin-bottom: 2rem;">${name}
+                                        </h2>
+                                        <p class="lead">${desc}</p>
+                                    </div>
+                                    <div class="col-md-5 order-md-1">
+                                        <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                                            width="500" height="500" src="${url}">
+                                    </div>
+                                </div>
+
+                                <hr class="featurette-divider">`;
+    mainContainer.appendChild(div);
+  }
+}
