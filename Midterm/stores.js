b73021cb9f405b802dd56bee9a3e29fd7eae059e
ColemanDDimmer <cdimmer@sundownbg.com>
2024-03-09 22:09:51 -0600

    Some small changes for general images for East Side Marketplace. Added convience store image generation.

---
+++ Midterm/stores.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  fetch("./data.json")
+    .then((response) => response.json())
+    .then((myData) => loadLocations(myData));
+});
+
+function loadLocations(myData) {
+  var mainContainer = document.getElementById("ConStoreLocs");
+  for (var i = 0; i < myData.ConvenienceStore.length; i++) {
+    let name = myData.ConvenienceStore[i].name;
+    let url = myData.ConvenienceStore[i].image;
+    let desc = myData.ConvenienceStore[i].desc;
+    // let url = myMovies.movies[i].url;
+    let div = document.createElement("div");
+    div.innerHTML = `
+                                <div class="row featurette">
+                                    <div class="col-md-7 order-md-2">
+                                        <h2 class="featurette-heading fw-normal lh-1"
+                                            style="text-align: center; margin-bottom: 2rem;">${name}
+                                        </h2>
+                                        <p class="lead">${desc}</p>
+                                    </div>
+                                    <div class="col-md-5 order-md-1">
+                                        <img class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto"
+                                            width="500" height="500" src="${url}">
+                                    </div>
+                                </div>
+
+                                <hr class="featurette-divider">`;
+    mainContainer.appendChild(div);
+  }
+}
