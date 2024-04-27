a074bfc8d6d651aa7dff587289a1fa664b9b88b3
ColemanDDimmer <cdimmer@sundownbg.com>
2024-04-27 18:24:40 -0500

    Delete page done

--- Assignment3/frontend/src/App.js
+++ Assignment3/frontend/src/App.js
@@ -263,7 +263,7 @@ function App() {
     ]);
 
     useEffect(() => {
-      fetch("http://127.0.0.1:8081/catalog")
+      fetch("http://127.0.0.1:8081/listProducts")
         .then((response) => response.json())
         .then((data) => {
           setProducts(data);
@@ -308,7 +308,159 @@ function App() {
   }
 
   function DeleteView() {
-    return <div>Delete View</div>;
+    const [index, setIndex] = useState(0);
+    const [products, setProducts] = useState([
+      {
+        id: "",
+        title: "",
+        price: "",
+        description: "",
+        category: "",
+        image: "",
+        rating: "",
+      },
+    ]);
+
+    useEffect(() => {
+      fetch("http://127.0.0.1:8081/listProducts")
+        .then((response) => response.json())
+        .then((data) => {
+          setProducts(data);
+          console.log("Load initial Catalog of Products in DELETE :", data);
+        });
+    }, []);
+
+    // Function to review products like carousel
+    function getOneByOneProductNext() {
+      if (products.length > 0) {
+        if (index === products.length - 1) setIndex(0);
+        else setIndex(index + 1);
+      }
+    }
+    // Function to review products like carousel
+    function getOneByOneProductPrev() {
+      if (products.length > 0) {
+        if (index === 0) setIndex(products.length - 1);
+        else setIndex(index - 1);
+      }
+    }
+
+    // Delete de product by its id <- id is Hook
+    const deleteOneProduct = (id) => {
+      console.log("Product to delete :", id);
+      fetch("http://localhost:8081/deleteProduct/" + id, {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ id: id }),
+      })
+        .then((response) => {
+          if (response.status != 200) {
+            return response.json().then((errData) => {
+              throw new Error(
+                `POST response was not ok :\n Status:${response.status}. \n Error: ${errData.error}`
+              );
+            });
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log("Delete a product completed : ", id);
+          console.log(data);
+          // reload products from the local products array
+          const newProducts = products.filter((product) => product.id !== id);
+          setProducts(newProducts);
+          setIndex(0);
+          // show alert
+          if (data) {
+            alert("Product Sucessfully deleted");
+          }
+        })
+        .catch((error) => {
+          console.error("Error adding item:", error);
+          alert("Error adding robot:" + error.message); // Display alert if there's an error
+        });
+    };
+
+    return (
+      <div className="container">
+        {/* Buttons to simulate carousel */}
+        <h3 class="text-center">Delete a product:</h3>
+        <div class="row">
+          <button class="btn col-1" onClick={() => getOneByOneProductPrev()}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="currentColor"
+              class="bi bi-arrow-left"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+              />
+            </svg>
+          </button>
+          <div class="col text-center" key={products[index].id}>
+            <img src={products[index].image} width={30} /> <br />
+            Id:{products[index].id} <br />
+            Title: {products[index].title} <br />
+            Category: {products[index].category} <br />
+            Price: {products[index].price} <br />
+            Rating :{products[index].rating.rate} (Averaged over{" "}
+            {products[index].rating.count} rating/s)
+            <br />
+          </div>
+          <button class="btn col-1" onClick={() => getOneByOneProductNext()}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="currentColor"
+              class="bi bi-arrow-right"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+              />
+            </svg>
+          </button>
+        </div>
+        <div class="row">
+          <div class="col-4"></div>
+          <button
+            class="btn border col-4"
+            onClick={() => deleteOneProduct(products[index].id)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+            </svg>
+            Delete
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              class="bi bi-trash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+            </svg>
+          </button>
+          <div class="col-4"></div>
+        </div>
+      </div>
+    );
   }
 
   function createHeader() {
